2d0ab0fd9be855012808c26a2d5818a79fcc4575
Jesse Shawl <jesse@jshawl.com>
2015-12-28 10:35:22 -0600

    evalin w/ codemirror

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 (function(){
-  var input = document.getElementById("console").innerHTML;
-  document.getElementById("input").innerHTML = input;
-  try{
-    input = input.replace(/console\.log/,"log")
-    var ret = eval(input)
-    output.innerHTML = output.innerHTML + "<br>>" +ret;
-  } catch (e) {
-    output.innerHTML += e; 
+  var cm = CodeMirror(document.getElementById("input"),{
+    value: document.getElementById("console").innerHTML,
+    mode: 'javascript',
+    lineNumbers: true
+  })
+  function evaluate(input){
+    try{
+      input = input.replace(/console\.log/,"log")
+      var ret = eval(input)
+      output.innerHTML = output.innerHTML + "<br>>" +ret;
+    } catch (e) {
+      output.innerHTML += e; 
+    }
+    function log( input ){
+      output.innerHTML = output.innerHTML + "<br><-"+input;
+    }
   }
-  function log( input ){
-    output.innerHTML = output.innerHTML + "<br><-"+input;
-  }
+
+  var run = document.querySelector(".js-run")
+  run.addEventListener("click", function(e){
+    e.preventDefault(); 
+    evaluate(cm.getValue())
+  })
 })()
