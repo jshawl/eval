cb3f86a2b8e6ee12c0a3cccdf3ed3e36065a97e2
Jesse Shawl <jesse@jshawl.com>
2015-12-29 23:13:45 -0500

    workin w/ db

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-(function(){
-  var value = atob(window.location.hash.substr(1)) || "function hello(){\n  return 'hey there';\n}\n\nhello();"
+  var value = "function hello(){\n  return 'hey there';\n}\n\nhello();"
+  var create = document.querySelector(".js-create");
+  var params = function(){
+    var params = window.location.hash.substr(1).split("/")
+    params.shift()
+    return {
+      versionId: params[1],
+      evalId: params[0]
+    }
+  }
+  if(params().versionId){
+    changeCreateToUpdate()
+    $.getJSON("http://localhost:3000/evals/" + params().evalId + "/versions/" + params().versionId)
+     .then(function(res){
+       console.log(res); 
+       cm.setValue(res.contents)
+       output.innerHTML = "";
+       evaluate(cm.getValue())
+     })
+  }else{
+    $.getJSON("http://localhost:3000/evals/" + params().evalId + "/versions/last")
+     .then(function(res){
+       console.log(res); 
+       cm.setValue(res.contents)
+     })
+  }
   var cm = CodeMirror(document.getElementById("input"),{
     value: value,
     mode: 'javascript',
@@ -9,7 +33,6 @@
   function evaluate(input){
     try{
       input = input.replace(/console\.log/g,"log")
-  console.log(input)
       var ret = JSON.stringify(eval(input))
       output.innerHTML = output.innerHTML + "<br>-> " +ret;
     } catch (e) {
@@ -32,15 +55,45 @@
     e.preventDefault(); 
     output.innerHTML = "";
   })
-
   var isTyping
   document.body.addEventListener("keyup", function(event){
     isTyping = clearTimeout( isTyping )
     isTyping = setTimeout(function(e){
-      save( cm.getValue() )
+      //save( cm.getValue() )
     },1000) 
   })
-  function save(val){
-    window.location.hash = btoa(val)
+  create.addEventListener("click", function(event){
+    event.preventDefault(); 
+    createEval(cm.getValue());
+  })
+  function createEval(val){
+    $.post("http://localhost:3000/evals", {
+      contents: val
+    },function(res){
+      changeCreateToUpdate()
+      window.location.hash = "/" + res.evalId + "/0";
+    })
   }
-})()
+  $("body").on("click", ".js-update", function(event){
+    event.preventDefault(); 
+    update(cm.getValue());
+  })
+  function create(val){
+    $.post("http://localhost:3000/evals", {
+      contents: val
+    },function(res){
+      window.location.hash = "/" + res.evalId + "/0";
+    })
+  }
+  function update(val){
+    $.post("http://localhost:3000/evals/" + params().evalId + "/versions", {
+      contents: val
+    },function(res){
+      console.log(res);
+      window.location.hash = "/" + res.version.evalId + "/" + (res.index - 1);
+    })
+  }
+  function changeCreateToUpdate(){
+    $(".js-create").remove();
+    $(".ctrl").append("<a href='' class='js-update'>Update</a>")
+  }
